6597e30253a07d4fc091542aa877f8e4d4e1dd0d
Oleksandr_Pataman <oleksandr_pataman@epam.com>
2022-07-22 13:18:02 +0200

    refactor: adjust response data property

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,7 @@
 
 const API_PATHS = {
-  product: 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com',
+  // product: 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com',
+  product: 'http://localhost:4000',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
