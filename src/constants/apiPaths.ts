390331967f632273a867c67d79e42d0440c7dd35
Oleksandr_Pataman <oleksandr_pataman@epam.com>
2022-07-22 13:34:58 +0200

    fix: use different api urls depends on env

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,8 @@
 
 const API_PATHS = {
-  // product: 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com',
-  product: 'http://localhost:4000',
+  product: process.env.NODE_ENV === 'development' ?
+    'http://localhost:4000' :
+    'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
