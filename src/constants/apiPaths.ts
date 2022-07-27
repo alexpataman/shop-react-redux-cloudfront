ee7cc0fcee3d6cadfddb46ef9f87651cd39b7f35
Oleksandr_Pataman <oleksandr_pataman@epam.com>
2022-07-27 14:43:00 +0200

    feat: implement file upload endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,11 +1,18 @@
+const getProductApiPath = () =>
+  'development'
+    ? 'http://localhost:4000'
+    : 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com';
+
+const getImportApiPath = () =>
+  'development'
+    ? 'http://localhost:4001'
+    : 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com';
 
 const API_PATHS = {
-  product: process.env.NODE_ENV === 'development' ?
-    'http://localhost:4000' :
-    'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com',
+  product: getProductApiPath(),
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: getImportApiPath(),
+  bff: getProductApiPath(),
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
