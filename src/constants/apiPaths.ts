594f59d951b5fe1cb6874285297c613dc075993e
Oleksandr_Pataman <oleksandr_pataman@epam.com>
2022-07-28 22:27:05 +0200

    fix: api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,12 @@
 const getProductApiPath = () =>
-  'development'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000'
     : 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com';
 
 const getImportApiPath = () =>
-  'development'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:4001'
-    : 'https://ob84jfvkmb.execute-api.eu-west-1.amazonaws.com';
+    : 'https://8wgz36e2h8.execute-api.eu-west-1.amazonaws.com';
 
 const API_PATHS = {
   product: getProductApiPath(),
